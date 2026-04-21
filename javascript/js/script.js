3d11e8da2d40fee501a5c909e101afaa0c41fc03
Rafaelbackend <rafaelvbarbosa@gmail.com>
2026-04-21 15:06:51 -0300

    Projeto final organizado

---
+++ javascript/js/script.js
@@ -0,0 +1,30 @@
+//const Nome = ' Rafael';
+//const Mouse = 10;
+//const Teclado = 25;
+//const Desconto = 10;
+//const msg = 'Olá, ${nome}! você comprou  '
+
+//console.log(nome)
+
+//const idade = 19;
+
+//if(idade > 18){
+//   alert("Você é maior de idade!")
+//}else if(idade > 12 && idade < 18){
+//   alert("Você é adolescente!")
+//}else{
+//    alert("Você é criança!")
+//}
+ 
+//const podeDirigir = 18;
+//const status = podeDirigir >= 18? "Voce pode dirigir":"Não pode dirigir";
+//console.log(status); // "Maior"
+
+//function criarUsuario(nome, idade, ativo = true) {
+// console.log(`${nome}, ${idade} anos, ativo: ${ativo}`);
+//}
+
+//criarUsuario('Maria', 25);       // "Maria, 25 anos, ativo: true"
+//criarUsuario('João', 30, false); // "João, 30 anos, ativo: false"
+//criarUsuario('Pedro',26)
+
