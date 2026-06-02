92b7a333d0f0f85dfd5846d6869436c12a42af20
Rafaelbackend <rafaelvbarbosa@gmail.com>
2026-06-02 19:20:53 -0300

    Organizando estrutura do projeto

--- public/js/app.js
+++
@@ -1,6 +0,0 @@
-const API = 'http://localhost:3000/produtos'
-const listaProdutos = document.getElementById('listaProdutos')
-const produtoForm = document.getElementById('produtoForm')
-const loginForm = document.getElementById('loginForm')
-const filtroCategoria = document.getElementById('filtroCategoria')
-let produtos = []
